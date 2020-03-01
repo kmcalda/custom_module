0dd6e221d4c9dbb52d98f0b898e37766a0c43840
kmcalda <caldakevinmarvin@yahoo.com>
2020-03-01 11:54:32 +0800

    [FIX] ALERT USER TO NOT CHANGE THE REGION

--- models/extended_models.py
+++ models/extended_models.py
@@ -13,11 +13,20 @@ class ContactsRespartnerInherited(models.Model):
 
     region_contact = fields.Char(string='Region')
 
+    # assign region_name to region_contact base on state_id
     @api.onchange('state_id')
     def onchange_value_state_id(self):
         for reg in self:
             reg.region_contact = reg.state_id.region_name
 
+    # alert user to NOT change the region
+    @api.constrains('region_contact')
+    def check_region_contact_validity(self):
+        for reg_cont in self:
+            if reg_cont.region_contact != reg_cont.state_id.region_name:
+                raise ValidationError("Error! You're not allowed to change the Region")
+
+    # alert user to put 12-digits only without special characters
     @api.constrains('vat')
     def _check_value_len(self):
         for val in self:
@@ -26,13 +35,15 @@ class ContactsRespartnerInherited(models.Model):
             if len(val.vat) != 12:
                 raise ValidationError("Error! TIN should be 12-digits!")
 
-    # overriding create of res.partner
+    # sample overriding create of res.partner
     @api.model
     def create(self, vals):
+        # code inside(for experiment)
         res = super(ContactsRespartnerInherited, self).create(vals)
-        # code inside
+        # code inside(for experiment)
         return res
 
+    # field ref and vat unique
     _sql_constraints = [
         ('ref_unique', 'unique(ref)',
          'Error! Reference already exist'),
@@ -45,6 +56,7 @@ class ContactsRespartnerInherited(models.Model):
 class ProductTemplateInherited(models.Model):
     _inherit = 'product.template'
 
+    # field default_code unique
     _sql_constraints = [
         ('default_code_unique', 'unique(default_code)',
          'Error! Internal reference already exist'),
